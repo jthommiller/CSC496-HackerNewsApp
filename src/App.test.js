0b72925702bc0614f8dcd05719431b717cf08199
James Miller <jthommiller@yahoo.com>
2020-04-13 17:48:46 -0700

    Added Enzyme Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import logo from './logo.svg';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+it('renders learn react link', () => {
+  const linkElement = shallow(<App/>);
+  expect(linkElement.find('a').text()).toEqual('Learn React')
 });
+
+it('renders image', () => {
+  const wrapper = shallow(<App/>);
+  expect(wrapper.find('img').prop("src")).toEqual(logo);
+})
