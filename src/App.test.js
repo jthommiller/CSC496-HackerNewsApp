735f4ac6d378c930f56f4d9058c9372e526a67f0
Chase Dawson <chase_dawson7@mymail.eku.edu>
2020-04-18 15:24:47 -0400

    Changed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import App, {Search, Table} from './App';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -19,14 +19,14 @@ it('Search button has text set to "Search"', () => {
 });
 
 describe('App', () => {
-  const wrapper = mount(<App/>);
+  const wrapper = shallow(<App/>);
   test('Creates Search', () => {
     expect(wrapper.find(Search).length).toEqual(1);
   });
 
-  test('Search button says "Search"', () => {
+  test('onSubmit does not crash', () => {
     const search = wrapper.find(Search);
-    expect(Search.text).toEqual('Search');
+    wrapper.find('Search').invoke('onSubmit')
   });
   
 });
